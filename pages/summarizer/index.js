06025198290764f784712bd92d189dae6e5ef027
qomarullah <qomarullah.mail@gmail.com>
2024-09-12 06:33:17 +0700

    add button main

--- pages/summarizer/index.js
+++ pages/summarizer/index.js
@@ -1,8 +1,27 @@
 import React from "react";
+import { useRouter } from "next/router";
 import AudioSummarizer from "@/components/AudioSummarizer";
 
 const SummarizerPage = () => {
-  return <AudioSummarizer />;
+  const router = useRouter();
+  const goBack = () => {
+    router.push("/");
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-between bg-gray-100 p-4">
+      {/* Main Translator Component */}
+      <AudioSummarizer />
+
+      {/* Go Back Button */}
+      <button
+        onClick={goBack}
+        className="px-4 py-2 mb-4 bg-gray-300 text-gray-800 rounded-lg shadow hover:bg-gray-400 transition"
+      >
+        Go To Main
+      </button>
+    </div>
+  );
 };
 
 export default SummarizerPage;
--- pages/translator/index.js
+++ pages/translator/index.js
@@ -1,8 +1,30 @@
 import React from "react";
 import AudioTranslator from "@/components/AudioTranslator";
+import { useRouter } from "next/router";
 
 const TranslatorPage = () => {
-  return <AudioTranslator />;
+  const router = useRouter();
+  const goBack = () => {
+    router.push("/");
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-between bg-gray-100 p-4">
+      {/* Go Back Button */}
+      
+
+      {/* Main Translator Component */}
+      <AudioTranslator />
+
+        {/* Go Back Button */}
+        <button
+        onClick={goBack}
+        className="px-4 py-2 mb-4 bg-gray-300 text-gray-800 rounded-lg shadow hover:bg-gray-400 transition"
+      >
+        Go To Main
+      </button>
+    </div>
+  );
 };
 
 export default TranslatorPage;
